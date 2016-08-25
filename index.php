1f345bb4a6ab86a0e893709fe2d99e6392928874
Fred McClurg <frmcclurg@gmail.com>
2016-08-25 07:32:29 -0500

    Add favicon and main pages.

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Techie Boot Camp: Table of Contents</title>
+
+   <meta name="description"
+      content="This is the Table of Contents to all the 'Techie Boot Camp'
+               courses on the web site.">
+
+   <meta name="keywords"
+      content="web, programming, application, development, course, training, overview, language">
+
+	<link rel="stylesheet" type="text/css" href="common/css/global.css">
+</head>
+
+<!-- ------------------------------------------------------------------ -->
+<!-- Name:     index.html                                               -->
+<!-- Created:  June 28, 2008                                            -->
+<!-- Author:   Fred R. McClurg                                          -->
+<!-- ------------------------------------------------------------------ -->
+
+<body bgcolor="#ffffff">
+
+<a name="top"></a>
+
+<h3 align="center">Techie Boot Camp</h3>
+<h2 align="center">Table of Contents</h2>
+
+<img src="common/images/pixels/ccccff.png" border=0 height=4 width="100%">
+
+<br><br>
+
+<dl>
+   <dd> <img src="common/images/icons/balls/16x16/ffff00.png">
+        <a href="courses">Introduction to Web Programming</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/ff0000.png">
+        <a href="courses/linux">Linux Boot Camp</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/0000ff.png">
+        <a href="courses/eclipse">Eclipse Overview</a> <br><br>
+
+   <!--
+   <dd> <img src="common/images/icons/balls/16x16/ffff00.png">
+        <a href="courses/css/lessons">CSS Boot Camp</a> <br><br>
+   -->
+
+   <dd> <img src="common/images/icons/balls/16x16/00ff00.png">
+        <a href="courses/html">HTML Boot Camp</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/ff00ff.png">
+        <a href="courses/javascript">JavaScript Boot Camp</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/00ffff.png">
+        <a href="courses/jquery">jQuery Boot Camp</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/ffff00.png">
+        <a href="courses/jqueryui">jQuery UI Boot Camp</a> <br><br>
+
+   <dd> <img src="common/images/icons/balls/16x16/a020f0.png">
+        <a href="courses/php">PHP/MySQL Boot Camp</a> <br><br>
+</dl>
+
+<!--
+<p> <br>
+
+<img src="common/images/pixels/ccccff.png" border=0 height=4 width="100%">
+
+<table>
+   <tr>
+      <td align=center valign=top width=80>
+         <a href="/" class="icons">
+         <img src="common/images/icons/home.gif"
+              border=1 alt="Home" title="Home Page"><br>
+         <font size=1>
+            Home<br>
+            Page
+         </font></a>
+      </td>
+
+      <td align=center valign=top width=80>
+         <a href="#top" class="icons">
+         <img src="common/images/icons/dkBlueUpArrow.png"
+              border=1 alt="Top" title="Top Page"><br>
+         <font size=1>
+            Top<br>
+            Page
+         </font></a>
+      </td>
+
+      <td align=center valign=top width=80>
+         <a href="previous.html" class="icons">
+         <img src="common/images/icons/dkBlueLtArrow.png"
+              border=1 alt="Previous" title="Previous Page"><br>
+         <font size=1>
+            Previous<br>
+            Page
+         </font></a>
+      </td>
+
+      <td align=center valign=top width=80>
+         <a href="next.html" class="icons">
+         <img src="common/images/icons/dkBlueRtArrow.png"
+              border=1 alt="Next" title="Next Page"><br>
+         <font size=1>
+            Next<br>
+            Page
+         </font></a>
+      </td>
+   </tr>
+</table>
+-->
+
+<hr>
+<p>
+
+<table width="100%">
+   <tr>
+      <td valign="bottom">
+         <a href='mailto:Fred%20R.%20McClurg%20%3Cfrmcclurg%40gmail.com%3E?subject=Techie%20Boot%20Camp%3A%20Table%20of%20Contents&body=Dear%20Fred%20R.%20McClurg%3A%0A%0A...'>
+         Fred R. McClurg</a>
+      </td>
+
+      <td align="right" valign="middle" nowrap>
+         <font size=2>
+            Last Modified:
+         </font>
+      </td>
+
+      <td width="10" valign="middle">
+         <nobr>
+         <font size=2>
+            <?=
+	            date( "F d Y H:i:s", getlastmod() );
+            ?>
+         </font>
+         </nobr>
+      </td>
+   </tr>
+
+   <tr>
+      <td colspan=4 align="center" class="copyright">
+         Copyright &copy; <?= date("Y"); ?> &nbsp;
+         Fred R. McClurg &nbsp;
+         All Rights Reserved.
+      </td>
+   </tr>
+</table>
+
+   <!-- Google Analytics -->
+   <script src="../../common/js/googleAnalytics.js"></script>
+</body>
+</html>
